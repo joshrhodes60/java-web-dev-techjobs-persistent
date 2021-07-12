ff414a105a68e5386ea0b0d9f7435e80e71d0b2a
“josh” <joshrhodes60@icloud.com>
2021-07-12 13:20:50 -0500

    complete final assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 ## Part 1: Test it with SQL
-
+SELECT * FROM tech_jobs.job;
 ## Part 2: Test it with SQL
-
+Select name from employer where location="St. Louis city";
 ## Part 3: Test it with SQL
-
+SET FOREIGN_KEY_CHECKS=0; DROP TABLE job; SET FOREIGN_KEY_CHECKS=1;
 ## Part 4: Test it with SQL
